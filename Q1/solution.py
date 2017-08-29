d6c7ba64fb647839ae3d961a1950843faa105d15
Sebastian Orozco <sebastianc1231@gmail.com>
2017-08-29 06:20:44 -0700

    solution.py

---
+++ Q1/solution.py
@@ -0,0 +1,14 @@
+from math import sqrt
+
+def answer(area):
+    tmp = area
+    res = []
+    while area > 0:
+        sq = sqrt(tmp)
+        if sq == int(sq):
+            res.append(tmp)
+            area -= tmp
+            tmp = area
+            continue
+        tmp -= 1
+    return res
